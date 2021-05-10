2e94ba31fe2b9ebbd0db28eaabc71d7820270db2
PeterT.Mikkelsen <petertmikkelsen3@gmail.com>
2021-05-10 11:05:08 +0200

    Tests
    Co-authored-by: Kristian <fkq970@alumni.ku.dk>

---
+++ BreakoutTests/BlockTests.cs
@@ -0,0 +1,7 @@
+namespace BreakoutTests
+{
+    public class BlockTests
+    {
+        
+    }
+}
--- BreakoutTests/UnitTest1.cs
+++ BreakoutTests/UnitTest1.cs
@@ -14,11 +14,18 @@ using DIKUArcade.Math;
 namespace BreakoutTests {
     public class PlayerTests {
         public Player player;
-        
+        private LevelLoader levelLoader;
+        //string path = Directory.GetCurrentDirectory();
+        //string path2 = Directory.GetParent(path);
+        bool retValMeta = true;
+        bool retValLegend = true;
+
         [SetUp]
         public void SetUp() {
             player = new Player(
                 new DynamicShape(new Vec2F(0.41f, 0.1f), new Vec2F(0.17f, 0.0225f)), null);
+            levelLoader = new LevelLoader();
+            levelLoader.LoadNewlevel(Path.Combine("Assets", "Levels", "level1.txt"));
         }
 
         [Test]
@@ -37,6 +44,40 @@ namespace BreakoutTests {
             }
             Assert.AreEqual(0.82f, player.shape.Position.X);
         }
-        
+        [Test]
+        // siden at vi sætter spiller billedet ind fra nederste venstre hjørne af figuren, skal vi sætte player.shape.Position.X til
+        // at være det halve af dens brede
+        public void SpawnsInMiddle() {
+            Assert.AreEqual(0.41f, player.shape.Position.X);
+            Assert.AreEqual(0.1f, player.shape.Position.Y);
+        }
+        [Test]
+        public void MapTest() {
+            Assert.AreEqual(25 , levelLoader.map.Length);
+        }
+        [Test]
+        // I denne test tjekker vi om string arrayet indeholder et ":" da meta delen af .txt filen er den eneste der indeholder sådan et tegn på hver linje.
+        public void MetaTest() {
+            foreach (string line in levelLoader.meta) {
+                if (!line.Contains(":")) {
+                    retValMeta = false;
+                }
+                Assert.Equals(true, retValMeta);
+            }
+            
+        }
+        [Test]
+        public void LegendTest() {
+            foreach (string line in levelLoader.legend) {
+                if (!line.Contains(")")) {
+                    retValLegend = false;
+                }
+                Assert.Equals(true, retValLegend);
+            }
+        }
+        [Test]
+        public void BlockTest() {
+
+        }
     }
 }
